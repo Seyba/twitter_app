d973e3227d7accca5992343534d2d3eb7c903fc0
Mahamadou <mmohmo@yahoo.com>
2023-09-07 10:36:31 -0400

    static route added

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.set('view engine', 'jsx')
 app.engine('jsx', jsxEngine()) 
 
 //* Middleware
+app.use(express.static('public'))
 app.use((req, res, next) => {
     //console.log('from middleware', req.method, req.url)
     next();
